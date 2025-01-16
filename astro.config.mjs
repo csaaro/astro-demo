b0a044244d0c4939836dfaae7ccce55f13060992
Christian Saaro <christian@saaro.de>
2025-01-16 22:56:00 +0100

    add base ath

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
       site: 'https://csaaro.github.io',
-      base: 'astro-demo',
+      base: '/astro-demo/',
 });
