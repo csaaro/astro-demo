4330f18d44ff2aee70660eb0ed4a24cd7c3fa667
Christian Saaro <christian@saaro.de>
2025-01-17 07:57:21 +0100

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-      site: 'https://csaaro.github.io/astro-demo/',
-      base: 'astro-demo'
+      site: 'https://csaaro.github.io',
+      base: 'astro-demo',
 });
